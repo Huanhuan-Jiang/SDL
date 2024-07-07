bb572de40afbf5555f7871a44ba6f54aa30c4d3e
huanhuan-jiang <jhuanhustc@gmail.com>
2024-07-07 12:02:17 -0700

    WIP

--- src/video/SDL_video.c
+++ src/video/SDL_video.c
@@ -531,7 +531,6 @@ const char *SDL_GetVideoDriver(int index)
 int SDL_VideoInit(const char *driver_name)
 {
     printf("Entering SDL_VideoInit\n");
-    //printf("Line %d: _this=%s\n", __LINE__, _this->name);
     SDL_VideoDevice *video;
     SDL_bool init_events = SDL_FALSE;
     SDL_bool init_keyboard = SDL_FALSE;
@@ -539,7 +538,6 @@ int SDL_VideoInit(const char *driver_name)
     SDL_bool init_touch = SDL_FALSE;
     int i = 0;
 
-    //printf("Line %d: _this=%s\n", __LINE__, _this->name);
     /* Check to make sure we don't overwrite '_this' */
     if (_this) {
         SDL_VideoQuit();
@@ -549,7 +547,6 @@ int SDL_VideoInit(const char *driver_name)
 
     /* Start the event loop */
     if (SDL_InitSubSystem(SDL_INIT_EVENTS) < 0) {
-        //printf("Line %d: video=%s\n", __LINE__, video->name);
         goto pre_driver_error;
     }
     init_events = SDL_TRUE;
@@ -568,9 +565,7 @@ int SDL_VideoInit(const char *driver_name)
 
     /* Select the proper video driver */
     video = NULL;
-    //printf("Line %d: _this=%s\n", __LINE__, _this->name);
     if (!driver_name) {
-        //printf("Line %d: _this=%s\n", __LINE__, _this->name);
         driver_name = SDL_GetHint(SDL_HINT_VIDEO_DRIVER);
         //printf("Line %d: _this=%s\n", __LINE__, _this->name);
     }
@@ -585,9 +580,7 @@ int SDL_VideoInit(const char *driver_name)
             for (i = 0; bootstrap[i]; ++i) {
                 if ((driver_attempt_len == SDL_strlen(bootstrap[i]->name)) &&
                     (SDL_strncasecmp(bootstrap[i]->name, driver_attempt, driver_attempt_len) == 0)) {
-                    printf("Line %d: i = %d, bootstrap[i] = %s\n", __LINE__, i, bootstrap[i]->name);
                     video = bootstrap[i]->create();
-                    printf("Line %d: video = %s\n", __LINE__, video->name);
                     break;
                 }
             }
@@ -597,7 +590,6 @@ int SDL_VideoInit(const char *driver_name)
     } else {
         for (i = 0; bootstrap[i]; ++i) {
             //printf("Line %d: video = %s\n", __LINE__, video->name);
-            printf("Line %d: i = %d, bootstrap[i] = %s\n", __LINE__, i, bootstrap[i]->name);
             video = bootstrap[i]->create();
             printf("Line %d: video = %s\n", __LINE__, video->name);
             if (video) {
